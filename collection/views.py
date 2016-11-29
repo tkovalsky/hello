9b519a9a718d8e825001968b62c415fc2caedc74
todd <t.e.kovalsky@gmail.com>
2016-11-29 12:50:44 -0500

    added links to detail pages

--- collection/views.py
+++ collection/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    things = Thing.objects.all()
+    return render(request, 'index.html', {
+        'things': things,
+	})
 
+def thing_detail(request, slug):
+    #get the objects
+    thing = Thing.objects.get(slug=slug)
+
+    #and pass the object to the template
+    return render(request, 'things/thing_detail.html', {
+        'thing':thing,
+    })
+
+    
