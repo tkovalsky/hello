d6d64ac941459dd222aee4ce335a012b48002abd
todd <t.e.kovalsky@gmail.com>
2016-11-29 17:27:57 -0500

    added update view

---
+++ collection/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from collection.models import Thing
+
+class ThingForm(ModelForm):
+    class Meta:
+        model = Thing
+        fields = ('name', 'description')
+
--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from collection.forms import ThingForm
 from collection.models import Thing
 
 # Create your views here.
@@ -18,4 +19,24 @@ def thing_detail(request, slug):
         'thing':thing,
     })
 
+def edit_thing(request, slug):
+    thing=Thing.objects.get(slug=slug)
+    form_class = ThingForm
+
+    if request.method == 'POST':
+        # grab the data from the submitted form and apply
+        # the form
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+	    #save the new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+	#other just create the form
+    else:
+        form = form_class(instance=thing)
+
+	#and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,})
     
