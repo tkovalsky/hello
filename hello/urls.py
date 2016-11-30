f60f53058d77cf93d29a40061b0d4c1eed925696
todd <t.e.kovalsky@gmail.com>
2016-11-30 11:43:47 -0500

    added registration

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete)
 from django.views.generic import TemplateView
+
 
 from collection import views
 
@@ -10,5 +16,25 @@ urlpatterns = [
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, name='thing_detail'),
     url(r'^things/(?P<slug>[-\w]+)/edit/$', views.edit_thing, name='edit_thing'),
+
+    #Account registration
+    url(r'^accounts/password/reset/$', password_reset,
+                                      {'template_name': 'registration/password_reset_form.html' },
+                                      name="password_reset"),
+
+    url(r'^accounts/password/reset/done/$', password_reset_done,
+                                      {'template_name': 'registration/password_reset_done.html' },
+                                      name="password_reset_done"),
+
+    url(r'accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+                                    password_reset_confirm,
+                                    {'template_name': 'registration/password_reset_confirm.html' },
+                                    name="password_reset_confirm"),
+
+    url(r'accounts/password/done/$', password_reset_complete,
+                                     {'template_name': 'registration/password_reset_complete.html'},
+                                     name="password_reset_complete"),
+
+    url(r'^accounts/', include('registration.backends.simple.urls' )),
     url(r'^admin/', admin.site.urls),
     ]
