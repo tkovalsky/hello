bbc70de4142bcc6fe0460fc35dfe4bb5507c2833
todd <t.e.kovalsky@gmail.com>
2016-11-30 12:51:28 -0500

    upated wsgi file

--- hello/wsgi.py
+++ hello/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello.settings_production")
 
 
 from django.core.wsgi import get_wsgi_application
